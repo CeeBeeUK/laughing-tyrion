12c80cba966b582328c2366d2451ed790a26537b
CeeBeeUK <colinbruce@gmail.com>
2025-10-27 12:13:30 +0000

    Add ingredient model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_16_162256) do
+ActiveRecord::Schema[8.1].define(version: 2025_10_27_115343) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
 
+  create_table "credentials", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "external_id"
+    t.string "nickname"
+    t.string "public_key"
+    t.bigint "sign_count"
+    t.datetime "updated_at", null: false
+    t.uuid "user_id"
+    t.index ["external_id"], name: "index_credentials_on_external_id", unique: true
+    t.index ["user_id"], name: "index_credentials_on_user_id"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "webauthn_id"
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  add_foreign_key "credentials", "users"
 end
---
+++ spec/models/ingredient_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Ingredient, type: :model do
+  subject(:ingredient) { Ingredient.build(name:) }
+
+  let(:name) { nil }
+
+  describe "validation" do
+    context "when the name is empty" do
+      it { is_expected.not_to be_valid }
+    end
+
+    context "when the name is present" do
+      let(:name) { "Bourbon" }
+
+      it { is_expected.to be_valid }
+    end
+
+    context "when the name already exists" do
+      before { Ingredient.create(name:) }
+
+      let(:name) { "Bourbon" }
+
+      it { is_expected.to_not be_valid }
+    end
+  end
+end
