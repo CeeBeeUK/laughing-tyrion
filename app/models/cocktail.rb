abe61dcff05693ff51a78e4af267f20343934d02
CeeBeeUK <colinbruce@gmail.com>
2025-10-27 16:28:56 +0000

    Update and create models for cocktail ingredient join

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,7 @@
 class Cocktail < ApplicationRecord
   has_and_belongs_to_many :tools
+  has_many :cocktails_ingredients
+  has_many :ingredients, through: :cocktails_ingredients
 
   validates :name, presence: true, uniqueness: true
 end
