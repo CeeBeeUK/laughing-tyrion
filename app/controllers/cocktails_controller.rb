b0354646601c5518884e1871efcc55af9bef7161
CeeBeeUK <colinbruce@gmail.com>
2025-11-01 12:07:56 +0000

    slight extension

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,42 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: %i[ show edit update]
   def index
     @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @cocktail.update(cocktail_params)
+        format.html { redirect_to @cocktail, notice: "Cocktail was successfully updated.", status: :see_other }
+        format.json { render :show, status: :ok, location: @cocktail }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @cocktail.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params.expect(:id))
+  end
+
+  def cocktail_params
+    params.expect(cocktail: [
+      :name,
+      :rating,
+      :notes,
+      :variations,
+      tool_ids: [],
+      cocktails_ingredients_attributes: [ [ :id, :quantity, :measurement, :ingredient_id ] ]
+    ])
   end
 end
--- spec/requests/cocktails_spec.rb
+++ spec/requests/cocktails_spec.rb
@@ -1,10 +1,88 @@
 require 'rails_helper'
 
-RSpec.describe "Cocktails", type: :request do
+RSpec.describe CocktailsController, type: :request do
   describe "GET /index" do
     it "returns http success" do
       get "/"
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    subject(:get_request) { get cocktail_path(cocktail) }
+
+    let(:cocktail) { Cocktail.create(name: "Gimlet") }
+
+    it "returns http success" do
+      get_request
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the expected page" do
+      get_request
+      expect(response.body).to include("Gimlet")
+    end
+  end
+
+  describe "GET /edit" do
+    subject(:get_request) { get edit_cocktail_path(cocktail) }
+
+    let(:cocktail) { Cocktail.create(name: "Gimlet") }
+
+    it "returns http success" do
+      get_request
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the expected page" do
+      get_request
+      expect(response.body).to include("Gimlet")
+    end
+  end
+
+  describe "PATCH /update" do
+    subject(:patch_request) { patch cocktail_path(cocktail, params:) }
+
+    let(:cocktail) { Cocktail.create(name: "Gimlet") }
+
+    let(:params) do
+      {
+        cocktail: {
+          notes: "this is a note"
+        }
+      }
+    end
+
+    context "when the params are valid" do
+      it "returns http redirect" do
+        patch_request
+        expect(response).to have_http_status(:redirect)
+      end
+
+      it "redirects to the show page" do
+        patch_request
+        expect(response).to redirect_to cocktail_path(cocktail)
+      end
+    end
+
+    context "when the params are invalid" do
+      let(:params) do
+        {
+          cocktail: {
+            name: ""
+          }
+        }
+      end
+
+      it "returns http unprocessable_entity status" do
+        patch_request
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it "redirects to the show page" do
+        patch_request
+        expect(response.body).to include("1 error prohibited this cocktail from being saved:")
+      end
     end
   end
 end
